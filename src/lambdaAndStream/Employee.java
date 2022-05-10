e59a1323057af17380b2622af3bd1be7a1f917e1
Ezugwu Joel <ezugwuugwu@gmail.com>
2022-05-10 11:54:53 +0100

    my personal commit code

--- src/lambdaAndStream/Employee.java
+++ src/lambdaAndStream/Employee.java
@@ -1,2 +1,56 @@
-package lambdaAndStream;public class Employee {
+package lambdaAndStream;
+
+public class Employee {
+    private String firstName;
+    private String lastName;
+    private double salary;
+    private String department;
+
+    public Employee(String firstName, String lastName, double salary, String department) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.salary = salary;
+        this.department = department;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public String getName(){
+        return firstName + " "+lastName;
+    }
+
+        @Override
+    public String toString() {
+        return String.format("%12s %12s %8s %8s", getFirstName(), getLastName(), getSalary(), getDepartment());
+    }
 }
